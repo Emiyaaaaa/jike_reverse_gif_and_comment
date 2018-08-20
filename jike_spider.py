1a110c799ebf7831fc66a58617231f1c49ebfc3f
Emiyaaaaa <2914034404@qq.com>
2018-08-20 15:52:53 +0800

    update

--- jike_spider.py
+++ jike_spider.py
@@ -3,8 +3,9 @@ import pickle
 from PIL import Image, ImageSequence
 import urllib.request
 import time
+import sys
 
-topic_id={
+topic_id_dict={
           '有趣的GIF动图':'5aa0c90f8e88bd00164daa3b',
           '不好笑便利店':'5701d10d5002b912000e588d'
           }
@@ -15,8 +16,9 @@ post_index = 0#动态下标，默认为0
 
 c = jike.JikeClient()
 
-for (topic,topic_id) in topic_id:
-    try:
+while True:
+    for (topic,topic_id) in topic_id_dict.items():
+        # try:
         topic_selected = c.get_topic_selected(topic_id = topic_id)
         #获取图片
         pictures = []
@@ -25,25 +27,12 @@ for (topic,topic_id) in topic_id:
                 pictures.append(item['picUrl'])
         #图片数量
         pic_num = len(pictures)
-    except BaseException:
-        print('爬取出错！')
-        continue
+        # except BaseException as e:
+        #     print('爬取出错！'+str(e))
+        #     continue
 
 
-    def comment():
-        #获取id
-        id = topic_selected[post_index].id
-
-        try:
-            with open(user_id_fn, 'rb') as file:
-                used_id = pickle.load(file)
-        except:
-            #记录文件不存在时创建
-            used_id = ['0']
-            with open(user_id_fn, 'wb') as file:
-                pickle.dump(used_id, file)
-
-        if id not in used_id:
+        def comment():
             msg = {}
             for i in range(pic_num):
                 pic = pictures[i]
@@ -59,52 +48,70 @@ for (topic,topic_id) in topic_id:
                     #评论
                     if i != 0:
                         time.sleep(6)
-                        if topic_id == topic_id['不好笑便利店']:
-                            #同时转发
-                            c.comment_it('',topic_selected[post_index],pictures='out.gif')
-                            msg['result'+str(i)] = 'succeed'
-                            msg['personal_updates'+str(i)] = 'true'
-                            msg['comment_time'+str(i)] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                        else:
-                            #不转发
-                            c.comment_it('',topic_selected[post_index],pictures='out.gif',sync2personal_updates=False)
-                            msg['result' + str(i)] = 'succeed'
-                            msg['personal_updates' + str(i)] = 'false'
-                            msg['comment_time'+str(i)] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                    if topic_id == topic_id_dict['不好笑便利店']:
+                        #同时转发
+                        c.comment_it('',topic_selected[post_index],pictures='out.gif')
+                        msg['result'+str(i)] = 'succeed'
+                        msg['personal_updates'+str(i)] = 'true'
+                        msg['comment_time'+str(i)] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                    else:
+                        #不转发
+                        c.comment_it('',topic_selected[post_index],pictures='out.gif',sync2personal_updates=False)
+                        msg['result' + str(i)] = 'succeed'
+                        msg['personal_updates' + str(i)] = 'false'
+                        msg['comment_time'+str(i)] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
                 except:
                     msg['result' + str(i)] = 'failed'
                     msg['comment_time' + str(i)] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
                     continue
-                #成功评论，将此消息id加入文件中
-                used_id.append(id)
-                msg['id'] = str[id]
-                with open(user_id_fn, 'wb') as file:
-                    pickle.dump(used_id, file)
 
             return msg
 
 
-    if pic_num != 0 and pic_num <= 3:
-        msg = comment()
-        #记录日志
-        with open('log.txt', 'a', encoding="utf-8") as fileobject:
-            fileobject.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-            li = [
-                  topic_selected[post_index].topic['content'],
-                  topic_selected[post_index].id,
-                  topic_selected[post_index].createdAt[:10]+' '+topic_selected[post_index].createdAt[11:19]
-                  ]
-            fileobject.write('\n\nTopic: {}\nID: {}\nRelease Time: {}'.format(*li))
-            for i in range[pic_num]:
-                if i == 0:
-                    fileobject.write('\nGif: '+pictures[i])
-                else:
-                    fileobject.write('\n\t '+pictures[i])
 
-            for i in range[pic_num]:
-                if i == 0:
-                    fileobject.write('\nComment Time: '+msg['result'+str(i)]+'  '+msg['comment_time'+str(i)])
-                else:
-                    fileobject.write('\n\t\t\t  '+msg['result'+str(i)]+'  '+msg['comment_time'+str(i)])
+        if pic_num != 0 and pic_num <= 3:
+            #获取id
+            id = topic_selected[post_index].id
+            try:
+                with open(user_id_fn, 'rb') as file:
+                    used_id = pickle.load(file)
+            except:
+                # 记录文件不存在时创建
+                used_id = ['0']
+                with open(user_id_fn, 'wb') as file:
+                    pickle.dump(used_id, file)
 
+            if id not in used_id:
+                try:
+                    msg = comment()
+                    print(msg)
+                except:
+                    sys.exit('评论发生未知错误')
+                #成功评论，将此消息id加入文件中
+                used_id.append(id)
+                msg['id'] = str(id)
+                with open(user_id_fn, 'wb') as file:
+                    pickle.dump(used_id, file)
+
+                #记录日志
+                with open('log.txt', 'a', encoding="utf-8") as fileobject:
+                    li = [
+                          topic_selected[post_index].topic['content'],
+                          topic_selected[post_index].id,
+                          topic_selected[post_index].createdAt[:10]+' '+topic_selected[post_index].createdAt[11:19]
+                          ]
+                    fileobject.write('\n\nTopic: {}\nID: {}\nRelease Time: {}'.format(*li))
+                    for i in range(pic_num):
+                        if i == 0:
+                            fileobject.write('\nGif: '+pictures[i])
+                        else:
+                            fileobject.write('\n\t '+pictures[i])
+
+                    for i in range(pic_num):
+                        if i == 0:
+                            fileobject.write('\nComment Time: '+msg['result'+str(i)]+'  '+msg['comment_time'+str(i)])
+                        else:
+                            fileobject.write('\n\t\t\t  '+msg['result'+str(i)]+'  '+msg['comment_time'+str(i)])
+
+        time.sleep(1)
     time.sleep(1)
